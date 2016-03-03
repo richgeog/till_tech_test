1c2a9349da01620619644e25844099277c160c01
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-03-03 18:40:18 +0000

    added test for calculating the total bill

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -5,4 +5,9 @@ describe Till do
     subject.add_item(:tea)
     expect(subject.items).to eq [:tea]
   end
+
+  it "calculates the total bill" do
+    subject.calculate_total(3.65)
+    expect(subject.total).to eq (3.65)
+  end
 end
