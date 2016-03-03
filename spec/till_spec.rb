40beac2193bdbe47a392bf2d792a2174368198a1
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-03-03 18:53:35 +0000

    added test for tax amount

--- spec/till_spec.rb
+++ spec/till_spec.rb
@@ -14,4 +14,8 @@ describe Till do
   it "starts the bill total at 0" do
     expect(subject.total).to eq (0)
   end
+
+  it "knows how much tax is" do
+    expect(subject.tax).to eq (8.64)
+  end
 end
