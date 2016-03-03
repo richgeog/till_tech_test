055649fe32f60194a8f54ea84d06e2b73807bcb2
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-03-03 18:44:32 +0000

    added calculate total method and test passing

--- lib/till.rb
+++ lib/till.rb
@@ -1,10 +1,19 @@
 class Till
-  attr_reader :items
+
+  BILL = 0
+
+  attr_reader :items, :total
+
   def initialize
     @items = []
+    @total = BILL
   end
 
   def add_item(product)
     items << product
+  end
+
+  def calculate_total(amount)
+    @total += amount
   end
 end
