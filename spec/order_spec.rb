3942858ef745ab47851ee742129f4d7c0df191c2
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-03-04 20:53:54 +0000

    added test for itemised bill

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -15,6 +15,13 @@ describe Order do
     expect(subject.total).to eq(3.65)
   end
 
+  it "can list items, quantities and price of the order" do
+    subject.add_item(:tea, 3.65)
+    subject.add_item(:tea, 3.65)
+    subject.add_item(:cafe_latte, 4.75)
+    expect(subject.itemised).to eq(:tea, '2 x 3.65',:cafe_latte, '1 x 4.75')
+  end
+
   it "calculates the tax amount" do
     subject.add_item(:tea, 3.65)
     expect(subject.tax_amount).to eq(0.32)
